67bffc8a6146055366524332bfc306df71ef6d8b
Peter McKasty <pmck25@yahoo.com>
2023-05-11 19:59:40 -0400

    created json string page elements for movie and restaurant history, created functions to render the movie and restaurant history

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-const API_KEY = '121fe711';
+const OMDb_API_KEY = '121fe711';
 
 // Get elements
 let findMovieButton = document.getElementById('find-movie-button');
@@ -10,3 +10,31 @@ let restaurantName = document.getElementById('restaurant-name');
 let movieHistoryList = document.getElementById('movie-history-list');
 let restaurantHistoryList = document.getElementById('restaurant-history-list');
 
+// Load movie history from local storage
+let movieHistory = JSON.parse(localStorage.getItem('movieHistory')) || [];
+
+// Load restaurant history from local storage
+let restaurantHistory = JSON.parse(localStorage.getItem('restaurantHistory')) || [];
+
+// Display movie history
+function displayMovieHistory() {
+  movieHistoryList.innerHTML = '';
+  for (let i = 0; i < movieHistory.length && i < 5; i++) {
+    const li = document.createElement('li');
+    li.textContent = movieHistory[i];
+	
+	// Add li to movie history list
+	movieHistoryList.appendChild(li);
+	}
+}
+
+// Display restaurant history
+function displayRestaurantHistory() {
+	restaurantHistoryList.innerHTML = '';
+	for (let i = 0; i < restaurantHistory.length && i < 5; i++) {
+	const li = document.createElement('li');
+	li.textContent = restaurantHistory[i];
+	// Add li to restaurant history list
+	restaurantHistoryList.appendChild(li);
+	}
+}
